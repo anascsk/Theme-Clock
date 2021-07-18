94eea281d529492a5d05f7e8ed4ee30e8716c2ec
anascsk <anascsk@gmail.com>
2021-07-18 23:32:36 +0530

    js

--- script.js
+++ script.js
@@ -37,4 +37,9 @@ function setTime() {
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 }
 
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 setTime()
