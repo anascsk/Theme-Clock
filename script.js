170c96d68273c83dd3d28803ce47f031d4dfb15f
anascsk <anascsk@gmail.com>
2021-07-18 22:48:44 +0530

    js

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ const toggleEl = document.querySelector(.toggle)
 
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
