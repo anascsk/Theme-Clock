c294845d5d21f0510c2be3ca27e1cf898d8000de
anascsk <anascsk@gmail.com>
2021-07-18 23:00:21 +0530

    js updates

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const toggleEl = document.querySelector(.toggle)
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHtml = 'Dark Mode'
+    }else {
+        html.classList.add('dark')
+        e.target.innerHtml = 'Light Mode'
+    }
+})
